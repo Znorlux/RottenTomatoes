615269fe291cd0b7bd604ad38778e5189f006dfc
Znorlux <mabe0898@hotmail.com>
2023-04-23 18:12:47 -0500

    Login Funcional

---
+++ RottenTomatoes/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RottenTomatoes.Controllers
+{
+    //Aqui solo creamos un Controlador el cual hereda de la clase Controller proveniente del framework mvc
+    public class AccountController : Controller
+    {
+        public ActionResult Login()//Este metodo debe llamarse tal cual como el archivo .cshtml del cual se cargará la pagina
+        {
+            //Este método de acción View() es
+            //responsable de buscar y devolver una vista (en este caso, la vista Login.cshtml).
+            return View();
+        }
+    }
+}
--- RottenTomatoes/Program.cs
+++ RottenTomatoes/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//mi perrito
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,4 +24,10 @@ app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//Cada que añadamos un nuevo controlador y view debemos llamar al metodo MapControllerRoute y
+//buscarlo tal que sea controlller = *nombre carpeta* y action = *archivo.cshtml* TODO guardado en la carpeta View
+app.MapControllerRoute(
+    name: "login",
+    pattern: "account/login",
+    defaults: new { controller = "Account", action = "Login" });
 app.Run();
