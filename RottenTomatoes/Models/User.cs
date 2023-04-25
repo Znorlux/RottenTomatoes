65308eff1fc209247bbd15b32eed149fba7a650a
Znorlux <mabe0898@hotmail.com>
2023-04-25 17:39:00 -0500

    Base de datos faltante

---
+++ RottenTomatoes/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RottenTomatoes.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo Contraseña es obligatorio.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "El campo Tipo de usuario es obligatorio.")]
+        public string UserType { get; set; }
+    }
+}
